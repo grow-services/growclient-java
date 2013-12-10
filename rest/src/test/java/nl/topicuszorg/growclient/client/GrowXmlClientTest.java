cc83bb7bdcea5b46c957d24393ac55be42bb7c2f
Dries Schulten <dries.schulten@gmail.com>
2013-12-10 14:16:32 +0100

    Mockito test.

---
+++ rest/src/test/java/nl/topicuszorg/growclient/client/GrowXmlClientTest.java
@@ -0,0 +1,53 @@
+package nl.topicuszorg.growclient.client;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.when;
+
+import nl.topicuszorg.growclient.model.request.Pregnacy;
+import nl.topicuszorg.growclient.model.response.GrowChart;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Date;
+
+
+/**
+ * XML Client tests
+ * 
+ * @author Dries Schulten
+ */
+public class GrowXmlClientTest
+{
+	@Mock
+	private GrowXmlClient client;
+
+	/**
+	 * Setup
+	 */
+	@Before
+	public void setup()
+	{
+		MockitoAnnotations.initMocks(this);
+	}
+
+	/**
+	 * Test call register function
+	 */
+	@Test
+	public void testRegisterPregancy()
+	{
+		GrowChart chart = new GrowChart();
+		chart.setGrowChartId("1");
+		chart.setGrowChartVersion("NL2013");
+
+		when(client.registerPregnancy(notNull(Pregnacy.class))).thenReturn(chart);
+
+		Pregnacy pregnacy = new Pregnacy();
+		pregnacy.setEdd(new Date());
+
+		assertNotNull(client.registerPregnancy(pregnacy));
+	}
+}
